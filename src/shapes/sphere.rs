7b9f83dc9b98af02ff81ed17726d4c3de1740152
wessamfathi <wessamfathi@gmail.com>
2025-04-05 23:28:44 +0200

    Fixed shadows

--- src/shapes/sphere.rs
+++ src/shapes/sphere.rs
@@ -11,39 +11,28 @@ pub struct Sphere {
 impl Sphere {
     pub fn hit(&self, r: Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
         let oc = r.origin - self.center;
-        let a = r.direction.length_squared();
-        let half_b = dot(oc, r.direction);
-        let c = oc.length_squared() - self.radius * self.radius;
-        let discriminant = half_b * half_b - a * c;
+		let a = dot(r.direction, r.direction);
+		let b = dot(oc, r.direction);
+		let c = dot(oc, oc) - self.radius * self.radius;
 
-        let mut result = true;
-
-        if discriminant < 0.0 {
-            false
-        } else {
-            let sqrtd = discriminant.sqrt();
-
-            // Find the nearest root that lie in the acceptable range
-            let mut root = (-half_b - sqrtd) / a;
-
-            if root < t_min || root > t_max {
-                root = (-half_b + sqrtd) / a;
-
-                if root < t_min || root > t_max {
-                    result = false;
-                }
-            }
-
-            if result {
-                rec.t = root;
-                rec.p = r.at(rec.t);
-                let outward_normal = (rec.p - self.center) / self.radius;
-                rec.set_face_normal(r, outward_normal);
-
-                true
-            } else {
-                false
-            }
-        }
+		let discriminant = b * b - a * c;
+		if discriminant > 0.0 {
+			let temp = (-b - discriminant.sqrt()) / a;
+			if temp < t_max && temp > t_min {
+				rec.t = temp;
+				rec.p = r.at(rec.t);
+				rec.normal = (rec.p - self.center) / self.radius;
+				return true;
+			}
+			
+			let temp = (-b + discriminant.sqrt()) / a;
+			if temp < t_max && temp > t_min {
+				rec.t = temp;
+				rec.p = r.at(rec.t);
+				rec.normal = (rec.p - self.center) / self.radius;
+				return true;
+			}
+		}
+		false
     }
 }
