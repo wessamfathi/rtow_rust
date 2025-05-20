a8941f607186d40139b3f23c68c0a0eecf83503d
Wessam <wessamfathi@gmail.com>
2025-05-20 12:56:47 +0200

    Fix normal orientation and record material

--- src/core/hit_record.rs
+++ src/core/hit_record.rs
@@ -24,7 +24,7 @@ impl HitRecord {
     }
 
     pub fn set_face_normal(&mut self, r: Ray, outward_normal: Vec3) {
-        self.front_face = dot(r.direction, outward_normal) > 0.0;
+        self.front_face = dot(r.direction, outward_normal) < 0.0;
         self.normal = if self.front_face {
             outward_normal
         } else {
